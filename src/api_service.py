e8f547e7ff01573c2a60c815aa27534458cda2f1
PrajualR <prajualramesh@gmail.com>
2026-01-26 19:30:40 +0530

    Fix for middleware

--- src/api_service.py
+++ src/api_service.py
@@ -55,12 +55,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:5500",
+        "http://localhost:4200",
         "http://localhost:5500",
-        "http://localhost:4200"
+        "http://127.0.0.1:5500",
     ],
-    allow_methods=["POST"],
-    allow_headers=["Content-Type"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
